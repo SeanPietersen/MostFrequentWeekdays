60831f0fee8cb5906c51cc3d0d3282619f8bfe0b
Sean Pietersen <seanpietersen7@gmail.com>
2022-03-16 09:39:40 +0200

    Feat: Added .App project

---
+++ MostFrequentWeekdays/MostFrequentWeekdays.App/WeekCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MostFrequentWeekdays.App
+{
+    public class WeekCalculator : IWeekCalculator
+    {
+        public string[] MostFrequentDays(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MostFrequentWeekdays/MostFrequentWeekdays.Test/WeekCalculatorTest.cs
+++ MostFrequentWeekdays/MostFrequentWeekdays.Test/WeekCalculatorTest.cs
@@ -1,3 +1,4 @@
+using MostFrequentWeekdays.App;
 using System;
 using Xunit;
 
